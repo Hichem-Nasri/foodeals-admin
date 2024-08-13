0d7a656e0add608239d0ac3ef4957a3f8d9c7693
geek <elmainiaymane03@gmail.com>
2024-08-13 13:27:17 +0100

    setup spring boot project
    add application properties in resources/application.yml

---
+++ src/test/java/net/foodeals/foodeals/FoodealsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.foodeals.foodeals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodealsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
