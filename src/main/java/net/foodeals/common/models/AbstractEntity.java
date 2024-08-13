51b9f80a4c46930d72f5ed4136ef2857488f4339
geek <elmainiaymane03@gmail.com>
2024-08-13 14:54:22 +0100

    add an abstract entity that contain the columns (created at, updated at)

---
+++ src/main/java/net/foodeals/common/models/AbstractEntity.java
@@ -0,0 +1,37 @@
+package net.foodeals.common.models;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * AbstractEntity
+ */
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class AbstractEntity<T> implements Serializable {
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @Getter
+    @Setter
+    private Instant createdAt = Instant.now();
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    @Getter
+    @Setter
+    private Instant updatedAt = Instant.now();
+
+    public abstract T getId();
+}
