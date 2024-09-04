32010a6705be825802959ed1318a1f111960efbb
Ziko909 <aabou.zakaria909@gmail.com>
2024-09-04 20:42:00 +0100

    feat : CRUD operations of rayon.

---
+++ foodeals-admin-api/src/main/java/net/foodeals/product/application/dtos/requests/CreateRayonDto.java
@@ -0,0 +1,8 @@
+package net.foodeals.product.application.dtos.requests;
+
+import lombok.Data;
+
+@Data
+public class CreateRayonDto {
+    private  String name;
+}
---
+++ foodeals-admin-api/src/main/java/net/foodeals/product/domain/repositories/RayonRepository.java
@@ -0,0 +1,9 @@
+package net.foodeals.product.domain.repositories;
+
+import net.foodeals.product.domain.entities.Rayon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface RayonRepository extends JpaRepository<Rayon, UUID> {
+}
