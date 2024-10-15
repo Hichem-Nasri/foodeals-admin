e3b2609fee1142140403cbf70b26fc2e3cdf8140
Ziko909 <aabou.zakaria909@gmail.com>
2024-10-15 18:35:07 +0100

    refact : change offer table

--- foodeals-admin-api/src/main/java/net/foodeals/offer/domain/entities/Offer.java
+++ foodeals-admin-api/src/main/java/net/foodeals/offer/domain/entities/Offer.java
@@ -2,6 +2,7 @@ package net.foodeals.offer.domain.entities;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
 import net.foodeals.common.models.AbstractEntity;
 import net.foodeals.common.valueOjects.Price;
 import net.foodeals.offer.domain.valueObject.Offerable;
@@ -16,6 +17,7 @@ import java.util.UUID;
 @Table(name = "offers")
 
 @Getter
+@Setter
 public class Offer extends AbstractEntity<UUID> {
 
         @Id
@@ -25,15 +27,15 @@ public class Offer extends AbstractEntity<UUID> {
 
         @Embedded
         @AttributeOverrides({
-                        @AttributeOverride(name = "amount", column = @Column(name = "price_amount")),
-                        @AttributeOverride(name = "currency", column = @Column(name = "price_currency"))
+                @AttributeOverride(name = "amount", column = @Column(name = "price_amount")),
+                @AttributeOverride(name = "currency", column = @Column(name = "price_currency"))
         })
         private Price price;
 
         @Embedded
         @AttributeOverrides({
-                        @AttributeOverride(name = "amount", column = @Column(name = "sale_price_amount")),
-                        @AttributeOverride(name = "currency", column = @Column(name = "sale_price_currency"))
+                @AttributeOverride(name = "amount", column = @Column(name = "sale_price_amount")),
+                @AttributeOverride(name = "currency", column = @Column(name = "sale_price_currency"))
         })
         private Price salePrice;
 
@@ -56,7 +58,13 @@ public class Offer extends AbstractEntity<UUID> {
         @Transient
         private IOfferChoice offerChoice;
 
-        Offer() {
+        @Embedded
+        private PublisherInfo publisherInfo;
+
+        @Transient
+        private PublisherI publisherI;
+
+        public Offer() {
         }
 
         public Offer setPrice(Price price) {
