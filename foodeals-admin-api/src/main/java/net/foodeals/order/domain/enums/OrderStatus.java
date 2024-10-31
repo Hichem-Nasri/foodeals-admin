33e4e0279eed1eaa6c9ceaa91100594722240f28
Ziko909 <aabou.zakaria909@gmail.com>
2024-10-31 17:31:15 +0100

    refact : add returned status in order status.

--- foodeals-admin-api/src/main/java/net/foodeals/order/domain/enums/OrderStatus.java
+++ foodeals-admin-api/src/main/java/net/foodeals/order/domain/enums/OrderStatus.java
@@ -1,5 +1,5 @@
 package net.foodeals.order.domain.enums;
 
 public enum OrderStatus {
-    IN_CART, OPEN, IN_PROGRESS, COMPLETED, CANCELED
+    IN_CART, OPEN, IN_PROGRESS, COMPLETED, CANCELED, RETURNED
 }
